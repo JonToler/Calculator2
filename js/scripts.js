2c1d68bef5ed98f35abb8ebebb0bf7d55b4960bb
Brian Dutz and Yusuf Qedan <yusuf@>
2016-08-08 09:58:30 -0700

    add readme, html, js files and css directory to project

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+/*
+var subtract = function(num1,num2){
+return num1 - num2;
+}
+var threeTimes = function(num1, num2, num3){
+	alert (num1*num2*num3);
+}
+var divide=function(num1,num2){
+	alert (num1/num2);
+}
+var remainder=function(num1, num2){
+	alert (num1%num2);
+}
+var multiply = function(num1, num2){
+	alert (num1*num2);
+}
+var number1 = prompt("Enter a Number");
+var number2 = prompt("Enter another Number");
+var number3 = prompt("Enter another Number");
+
+number1 = parseInt(number1);
+number2 = parseInt(number2);
+number3 = parseInt(number3);
+
+alert("The difference is " + subtract(number1,number2));
+multiply(number1, number2);
+threeTimes(number1,number2,number3);
+divide(number1,number2);
+remainder(number1,number2);
+*/
+var myFunction = function(str1,str2,str3){
+alert("your age is " +str1+ "your name is " +str2+ " your favorite food is "+ str3);
+}
+var inputtedAge = prompt("Enter your age");
+var inputtedName = prompt("Enter your Name");
+var inputtedFood = prompt("Enter your favorite food");
+myFunction(inputtedAge,inputtedName,inputtedFood);
